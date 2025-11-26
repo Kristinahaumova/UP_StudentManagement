25aa0443826967a052c002a0f43d3c45b753e46a
Kristinahaumova <kristinahaumova@gmail.com>
2025-11-26 14:40:57 +0500

    Настроила страницу MainWindow и создала ещё моделей. Хаюмова

---
+++ UP_Student_Management/Classes/Models/Student.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UP_Student_Management.Classes.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string Firstname { get; set; }
+        public string Surname { get; set; }
+        public string Patronomyc { get; set; }
+        public DateTime BirthDate { get; set; }
+        public int DepartmentId { get; set; }
+        public Department Department { get; set; }
+        public int? RoomId { get; set; }
+        public Room Room { get; set; }
+        public List<StudentCategoryLink> StudentCategories { get; set; }
+    }
+}
---
+++ UP_Student_Management/Classes/Models/StudentCategoryLink.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UP_Student_Management.Classes.Models
+{
+    public class StudentCategoryLink
+    {
+        public int Id { get; set; }
+        public int StudentId { get; set; }
+        public Student Student { get; set; }
+        public int CategoryId { get; set; }
+        public StudentCategory Category { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime? EndDate { get; set; }
+    }
+}
